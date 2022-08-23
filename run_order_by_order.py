a50740af544dd9980ed6f1df35d59afcca2dc550
0honus0 <63273720+0honus0@users.noreply.github.com>
2022-08-23 10:44:50 +0800

    update

--- run_order_by_order.py
+++ run_order_by_order.py
@@ -1,5 +1,6 @@
 import os
 from multiprocessing import Pool
+from time import sleep
 
 path = "D:\\"
 run_file_name = "1.py"
@@ -16,18 +17,20 @@ for root , dirs , files in os.walk(path , topdown = False):
 run_files = set(run_files)
 # print(run_files)
 
-def run(name):
-    #print(name)
+def run(name , i):
+    #print(f"sleep {i}")
+    sleep( i * 5)
     os.chdir(name)
     res = os.popen(f"python {run_file_name} > {run_file_name}.log")
     res = res.read()
 
 if __name__ == "__main__":
-    p = Pool(8)
+    #print(len(run_files))
+    p = Pool(len(run_files))
 
     index = 1
     for file in run_files:
-        p.apply_async(run , args = (file,))
+        p.apply_async(run , args = (file,index,))
         print(index)
         index+=1
 
