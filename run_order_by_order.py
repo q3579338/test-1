28cb7957f0883a9a90be6bf33091ee891d73d743
0honus0 <63273720+0honus0@users.noreply.github.com>
2022-08-23 10:16:14 +0800

    update

--- run_order_by_order.py
+++ run_order_by_order.py
@@ -1,4 +1,5 @@
 import os
+from multiprocessing import Pool
 
 path = "D:\\"
 run_file_name = "1.py"
@@ -9,15 +10,21 @@ for root , dirs , files in os.walk(path , topdown = False):
     for name in files:
         file = os.path.join(root, name)
         if run_file_name == name:
-            #print(file)
+            # print(file)
             run_files.append(root)
 
 run_files = set(run_files)
-#print(run_files)
+# print(run_files)
 
-for file in run_files:
-    os.chdir(file)
-    # try:
+def run(name):
+    os.chdir(name)
     os.popen(f"python {run_file_name} > {run_file_name}.log")
-    # except Exception as e:
-    #     print(e)
+
+if __name__ == "__main__":
+    p = Pool(8)
+
+    for file in run_files:
+        p.apply_async(run , args = (file,))
+
+    p.close()
+    p.join()
