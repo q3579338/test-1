c98b9d59391811de55701217969177fb6f8ed7f0
0honus0 <63273720+0honus0@users.noreply.github.com>
2022-08-23 10:02:50 +0800

    update

--- run_order_by_order.py
+++ run_order_by_order.py
@@ -1,19 +1,23 @@
 import os
 
 path = "D:\\"
-name = "getbalance.py"
+run_file_name = "1.py"
 
 run_files = []
 
 for root , dirs , files in os.walk(path , topdown = False):
     for name in files:
         file = os.path.join(root, name)
-        if name in file:
+        if run_file_name == name:
+            #print(file)
             run_files.append(root)
 
 run_files = set(run_files)
-# print(run_files)
+#print(run_files)
 
 for file in run_files:
     os.chdir(file)
-    os.popen(f"python {name}")
+    # try:
+    os.popen(f"python {run_file_name} > {run_file_name}.log")
+    # except Exception as e:
+    #     print(e)
